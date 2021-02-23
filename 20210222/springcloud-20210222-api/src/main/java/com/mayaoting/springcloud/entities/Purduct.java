d82a14c81373479615c2b19165370fbb9cd90774
mayaoting <15128197972@163.com>
2021-02-23 15:35:52 +0800

    重新提交

---
+++ 20210222/springcloud-20210222-api/src/main/java/com/mayaoting/springcloud/entities/Purduct.java
@@ -0,0 +1,33 @@
+package com.mayaoting.springcloud.entities;
+
+import java.io.Serializable;
+
+public class Purduct implements Serializable {
+    private String pid;
+    private String productName;
+    private String dbSource;
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDbSource() {
+        return dbSource;
+    }
+
+    public void setDbSource(String dbSource) {
+        this.dbSource = dbSource;
+    }
+}
